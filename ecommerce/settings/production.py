04df2cd20aaabf970588bfee20ac276bc127fe7e
Miguel Soto <msoto056@gmail.com>
2021-03-12 09:54:52 -0600

    DB Settings included

--- ecommerce/settings/production.py
+++ ecommerce/settings/production.py
@@ -108,6 +108,11 @@ DATABASES = {
     }
 }
 
+# add this
+import dj_database_url
+db_from_env = dj_database_url.config()
+DATABASES['default'].update(db_from_env)
+DATABASES['default']['CONN_MAX_AGE'] = 500
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
